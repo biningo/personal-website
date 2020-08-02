9abb3785f529a4fb54b5be02c4aebbf1c89a9ebd
biningo <biningo@yeah.net>
2020-08-02 21:20:48 +0800

    init:初始化前台展示模块blog-app

---
+++ blog-app/src/test/java/cn/bingo/blogapp/BlogAppApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.bingo.blogapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
