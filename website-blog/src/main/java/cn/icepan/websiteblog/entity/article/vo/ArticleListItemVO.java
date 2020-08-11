b704ce6369a315b9b8922a4390e97e511b3c8cbc
biningo <biningo@yeah.net>
2020-08-11 14:28:05 +0800

    ArticleListItemVo and ArticleUploadVo

---
+++ website-blog/src/main/java/cn/icepan/websiteblog/entity/article/vo/ArticleListItemVO.java
@@ -0,0 +1,13 @@
+package cn.icepan.websiteblog.entity.article.vo;/*/**
+ *@Author icepan
+ *@Date 2020/8/11 下午2:04
+ *@Description
+ *
+ **/
+
+import lombok.Data;
+
+@Data
+public class ArticleListItemVO extends ArticleBaseVO {
+    private String id;
+}
