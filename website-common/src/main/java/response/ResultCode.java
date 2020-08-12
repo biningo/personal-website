aa20d0c349916dd0db083e4e11e4928e84d91d5d
biningo <biningo@yeah.net>
2020-08-12 15:45:03 +0800

    返回的状态码

---
+++ website-common/src/main/java/response/ResultCode.java
@@ -0,0 +1,14 @@
+package response;
+
+/***
+ *@Author icepan
+ *@Date 2020/8/12 下午3:39
+ *@Description
+ *
+ ***/
+
+
+public class ResultCode {
+    private ResultCode(){}
+    public static final Integer SUCCESS=200;
+}
