8b30a13607324f44ac2420379f5c6dd49a50574c
biningo <biningo@yeah.net>
2020-08-12 23:10:00 +0800

    blog admin service

---
+++ website-blog/src/main/java/cn/icepan/websiteblog/BlogAdmin/service/BlogAdminService.java
@@ -0,0 +1,5 @@
+package cn.icepan.websiteblog.BlogAdmin.service;
+
+public interface BlogAdminService {
+
+}
---
+++ website-blog/src/main/java/cn/icepan/websiteblog/BlogAdmin/service/impl/BlogAdminServiceImpl.java
@@ -0,0 +1,14 @@
+package cn.icepan.websiteblog.BlogAdmin.service.impl;
+
+import cn.icepan.websiteblog.BlogAdmin.service.BlogAdminService;
+
+/***
+ *@Author icepan
+ *@Date 2020/8/12 下午4:06
+ *@Description
+ *
+ ***/
+
+
+public class BlogAdminServiceImpl implements BlogAdminService {
+}
