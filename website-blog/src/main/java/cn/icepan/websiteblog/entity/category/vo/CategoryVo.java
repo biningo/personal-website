76b7447e4af8aa08fb29843c6730084672ee5ad4
biningo <biningo@yeah.net>
2020-08-25 19:56:02 +0800

    category vo

---
+++ website-blog/src/main/java/cn/icepan/websiteblog/entity/category/vo/CategoryVo.java
@@ -0,0 +1,16 @@
+package cn.icepan.websiteblog.entity.category.vo;
+
+import lombok.Data;
+
+/***
+ *@Author icepan
+ *@Date 2020/8/25 下午7:31
+ *@Description
+ *
+ ***/
+
+@Data
+public class CategoryVo {
+    private String title;
+    private Integer count;
+}
